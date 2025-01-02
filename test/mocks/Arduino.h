97def5a068993976c9a418f097c9978a870367cb
peteGSX <peteracole@outlook.com.au>
2025-01-02 19:34:41 +1000

    Add mock DCCEXProtocol

--- test/mocks/Arduino.h
+++ test/mocks/Arduino.h
@@ -1,5 +1,5 @@
 /*
- *  © 2024 Peter Cole
+ *  © 2025 Peter Cole
  *
  *  This is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
---
+++ test/mocks/DCCEXProtocol.h
@@ -0,0 +1,55 @@
+/*
+ *  © 2025 Peter Cole
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this code.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef DCCEXPROTOCOL_H
+#define DCCEXPROTOCOL_H
+
+#include <gmock/gmock.h>
+
+/**
+ * @brief Mock DCCEXProtocol class
+ */
+
+enum TrackPower {
+  PowerOff = 0,
+  PowerOn = 1,
+  PowerUnknown = 2,
+};
+
+// Valid TrackManager types
+enum TrackManagerMode {
+  MAIN, // Normal DCC track mode
+  PROG, // Programming DCC track mode
+  DC,   // DC mode
+  DCX,  // Reverse polarity DC mode
+  NONE, // Track is unused
+};
+
+class Loco;
+
+class Turnout;
+
+class Route;
+
+class Turntable;
+
+class DCCEXProtocol {
+public:
+private:
+};
+
+#endif // DCCEXPROTOCOL_H
